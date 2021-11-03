bf0f3ddf763fc533a220b3cbe483a7f0f141f8d7
James Yaro <onuhjamesyaro@gmail.com>
2021-11-03 13:31:43 +0100

    80% design of the personal information form

--- cv-project/src/Components/PersonalInfo.js
+++ cv-project/src/Components/PersonalInfo.js
@@ -5,7 +5,7 @@ function PersonalInfo(props) {
     const {personal, handleChange} = props
     return (
         <section>
-            <p>Provide the correct information</p>
+            <p className='pp'>Provide the correct information</p>
             <div className='field-field'>
                 <div className='field'>
                     <label>First Name</label>
